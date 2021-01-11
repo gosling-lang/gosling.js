316cca1aec524ba9531ecb02b0f96d4ebef62df3
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2021-01-11 13:10:20 -0500

    refactor: fix tests

--- test/core/mark/index.test.ts
+++ test/core/mark/index.test.ts
@@ -1,8 +1,9 @@
 import { drawMark } from '../../../src/core/mark';
 import { GeminidTrackModel } from '../../../src/core/geminid-track-model';
+import { BasicSingleTrack } from '../../../src/core/geminid.schema';
 
 describe('Should draw marks correctly', () => {
     it('Should return early when some of parameters are not properly provided', () => {
-        drawMark(null, null, null, new GeminidTrackModel({}, []));
+        drawMark(null, null, null, new GeminidTrackModel({} as BasicSingleTrack, []));
     });
 });
--- test/core/utils/spec-preprocess.test.ts
+++ test/core/utils/spec-preprocess.test.ts
@@ -1,4 +1,4 @@
-import { GeminidSpec } from '../../../src/core/geminid.schema';
+import { GeminidSpec, Track } from '../../../src/core/geminid.schema';
 import { fixSpecDownstream } from '../../../src/core/utils/spec-preprocess';
 
 describe('Spec Preprocess', () => {
@@ -13,7 +13,11 @@ describe('Spec Preprocess', () => {
     });
 
     it('circular layout', () => {
-        const spec: GeminidSpec = { layout: 'circular', arrangement: { direction: 'horizontal' }, tracks: [{}] };
+        const spec: GeminidSpec = {
+            layout: 'circular',
+            arrangement: { direction: 'horizontal' },
+            tracks: [{} as Track]
+        };
         fixSpecDownstream(spec);
         expect(spec.tracks[0].layout).toEqual('circular');
     });
