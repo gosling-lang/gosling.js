34bcaa7b89fffe90ecb5fe777c9e450a82811866
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2021-01-24 10:19:10 -0500

    feat: make plugin axis and brush tracks visible in the package

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,7 @@ export { compile } from './core/compile';
 export { validateGoslingSpec } from './core/utils/validate';
 
 export { GoslingTrack } from './gosling-track';
+export { AxisTrack } from './axis-track';
+export { BrushTrack } from './higlass-brush-track';
 export { CSVDataFetcher } from './higlass-csv-datafetcher';
 export { RawDataFetcher } from './higlass-raw-datafetcher';
