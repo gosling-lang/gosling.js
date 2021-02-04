b0bdd5233f46c9316aa81811f472ac8a331ebc10
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2021-02-04 15:41:26 -0500

    test: tests for assembly

---
+++ src/core/utils/assembly.test.ts
@@ -0,0 +1,19 @@
+import { getChromInterval, getChromTotalSize, GET_CHROM_SIZES } from './assembly';
+import { CHROM_SIZE_HG38 } from './chrom-size';
+
+describe('Assembly', () => {
+    it('Random chromosome name', () => {
+        expect(GET_CHROM_SIZES().total).toBeDefined();
+        expect(GET_CHROM_SIZES('chrmosome1').total).toBeDefined();
+        expect(GET_CHROM_SIZES('random').total).toBeDefined();
+    });
+    it('hg38 ChromSizes', () => {
+        expect(GET_CHROM_SIZES('hg38').size).toEqual(CHROM_SIZE_HG38);
+    });
+    it('Chromosome total size calculation', () => {
+        expect(getChromTotalSize({ 1: 1, 2: 999 })).toEqual(1000);
+    });
+    it('Chromosome interval calculation', () => {
+        expect(getChromInterval({ 1: 111, 2: 222 })).toEqual({ 1: [0, 111], 2: [111, 333] });
+    });
+});
