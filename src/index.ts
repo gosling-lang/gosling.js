32c4e00fb170925ccce41ec37e255e64f9f37261
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2021-02-07 14:36:47 -0500

    refactor: make plugin data fetchers and tracks invisible from the package

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,3 @@ export const version = pkg.version;
 export { init } from './core/init';
 export { compile } from './core/compile';
 export { validateGoslingSpec } from './core/utils/validate';
-
-export { GoslingTrack } from './gosling-track';
-export { AxisTrack } from './axis-track';
-export { BrushTrack } from './higlass-brush-track';
-export { CSVDataFetcher } from './higlass-csv-datafetcher';
-export { RawDataFetcher } from './higlass-raw-datafetcher';
