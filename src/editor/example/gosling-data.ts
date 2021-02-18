b6bdfd6eb2f92bee7284838ef4baf3e48793c5cd
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2021-02-18 00:05:19 -0500

    chore: add a semantic zoom example for variant info

--- src/editor/example/gosling-data.ts
+++ src/editor/example/gosling-data.ts
@@ -3,7 +3,7 @@ export const GOSLING_PUBLIC_DATA = {
     fasta: 'https://resgen.io/api/v1/tileset_info/?d=WipsnEDMStahGPpRfH9adA',
     geneAnnotation: 'https://higlass.io/api/v1/tileset_info/?d=OHJakQICQD6gTD7skx4EWA',
     interaction: 'https://resgen.io/api/v1/tileset_info/?d=JzccFAJUQEiz-0188xaWZg',
-    clinvar: 'https://cgap-higlass.com/api/v1/tileset_info/?d=clinvar_20200824_hg38',
+    clinvar: 'https://server.gosling-lang.org/api/v1/tileset_info/?d=clinvar',
     region: 'https://resgen.io/api/v1/gt/paper-data/tileset_info/?d=SYZ89snRRv2YcxRwG_25_Q',
     region2: 'https://resgen.io/api/v1/gt/paper-data/tileset_info/?d=HT4KNWdTQs2iN477vqDKWg'
 };
