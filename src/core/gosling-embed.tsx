805fd3d6158fe9f26cf2612a4dda98849982014a
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2021-07-12 18:04:00 -0400

    feat: support more parameters in embed() function (#435)

--- src/core/gosling-embed.tsx
+++ src/core/gosling-embed.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Theme } from '..';
 import { GoslingComponent } from './gosling-component';
 import { GoslingSpec } from './gosling.schema';
 
@@ -8,8 +9,30 @@ import { GoslingSpec } from './gosling.schema';
  * @param element
  * @param spec
  */
-export function embed(element: HTMLElement, spec: GoslingSpec) {
+export function embed(
+    element: HTMLElement,
+    spec: GoslingSpec,
+    options?: {
+        padding?: number;
+        margin?: number | string;
+        border?: string;
+        id?: string;
+        className?: string;
+        theme?: Theme;
+    }
+) {
     // const ref = React.createRef();
-    ReactDOM.render(<GoslingComponent spec={spec} />, element);
+    ReactDOM.render(
+        <GoslingComponent
+            spec={spec}
+            padding={options?.padding}
+            margin={options?.margin}
+            border={options?.border}
+            id={options?.id}
+            className={options?.className}
+            theme={options?.theme}
+        />,
+        element
+    );
     // return ref.current;
 }
