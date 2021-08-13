8bcbe7ef3f0b0b3a7b01d406131a6ae31a1d50f3
Trevor Manz <trevor.j.manz@gmail.com>
2021-08-13 11:34:29 -0400

    refactor: create higlass-wrapper-component (#456)

---
+++ src/core/gosling-embed.ts
@@ -0,0 +1,89 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { GoslingSpec } from './gosling.schema';
+import { HiGlassSpec } from './higlass.schema';
+
+import { validateGoslingSpec } from './utils/validate';
+import { compile } from './compile';
+import { getTheme, Theme } from './utils/theme';
+import { GoslingTemplates } from './utils/template';
+import { GoslingApi, createApi } from './api';
+
+import { HiGlassApi, HiGlassComponentWrapper, HiGlassComponentWrapperProps } from './higlass-component-wrapper';
+
+export type GoslingEmbedOptions = Omit<HiGlassComponentWrapperProps['options'], 'background'> & {
+    id?: string;
+    className?: string;
+    theme?: Theme;
+};
+
+const MAX_TRIES = 20;
+const INTERVAL = 200; // ms
+
+// https://github.com/higlass/higlass/blob/0299ae1229fb57e0ca8da31dff58003c3e5bf1cf/app/scripts/hglib.js#L37A
+const launchHiglass = (
+    element: HTMLElement,
+    viewConfig: HiGlassSpec,
+    size: { width: number; height: number },
+    opts: GoslingEmbedOptions & { background: string }
+): Promise<HiGlassApi> => {
+    const ref = React.createRef<HiGlassApi>();
+    const component = React.createElement(HiGlassComponentWrapper, {
+        ref,
+        viewConfig,
+        size,
+        id: opts.id,
+        className: opts.className,
+        options: opts
+    });
+    ReactDOM.render(component, element);
+
+    // For some reason our wrapper component fails to initialize the provided `ref`
+    // immediately like `hglib.launch()`. This is a work-around to poll `ref`
+    // until it is initialized by our wrapper. We return a promise for the API once
+    // it is defined or raise an error.
+    // https://github.com/gosling-lang/gosling.js/pull/456#discussion_r687861694
+    return new Promise((resolve, reject) => {
+        let tries = 0;
+        const poll = setInterval(() => {
+            if (ref && ref.current) {
+                clearInterval(poll);
+                resolve(ref.current);
+            }
+            if (tries >= MAX_TRIES) {
+                reject(new Error('Failed to initialize HiGlassApi.'));
+            }
+            tries++;
+        }, INTERVAL);
+    });
+};
+
+/**
+ * Embed a Gosling component to a given HTMLElement.
+ * @param element
+ * @param spec
+ */
+export function embed(element: HTMLElement, spec: GoslingSpec, opts: GoslingEmbedOptions = {}) {
+    return new Promise<GoslingApi>((resolve, reject) => {
+        const valid = validateGoslingSpec(spec);
+
+        if (valid.state === 'error') {
+            reject(new Error('Gosling spec is not valid. Please refer to the console message.'));
+        }
+
+        const theme = getTheme(opts.theme || 'light');
+        const options = { ...opts, background: theme.root.background };
+
+        compile(
+            spec,
+            async (hsSpec, size) => {
+                const hg = await launchHiglass(element, hsSpec, size, options);
+                const api = createApi(hg, hsSpec, theme);
+                resolve(api);
+            },
+            [...GoslingTemplates],
+            theme
+        );
+    });
+}
---
+++ src/core/higlass-component-wrapper.tsx
@@ -0,0 +1,101 @@
+/* eslint-disable react/prop-types */
+import * as PIXI from 'pixi.js';
+import React, { useEffect, useState, forwardRef } from 'react';
+import uuid from 'uuid';
+
+import * as gosling from '..';
+// @ts-ignore
+import { HiGlassComponent } from 'higlass';
+import { HiGlassSpec } from './higlass.schema';
+
+/**
+ * Register plugin tracks and data fetchers to HiGlass. This is necessary for the first time before using Gosling.
+ */
+gosling.init();
+
+// TODO: Complete the API
+export type HiGlassApi = {
+    api: Record<string, any>;
+    pixiRenderer: PIXI.Renderer;
+    pixiStage: PIXI.IRenderableObject;
+};
+
+export interface HiGlassComponentWrapperProps {
+    size: { width: number; height: number };
+    viewConfig?: HiGlassSpec;
+    options: {
+        padding?: number;
+        margin?: number | string;
+        border?: string;
+        background?: string;
+    };
+    id?: string;
+    className?: string;
+}
+
+export const HiGlassComponentWrapper = forwardRef<HiGlassApi | undefined, HiGlassComponentWrapperProps>(
+    (props, ref) => {
+        // div `id` and `className` for detailed customization
+        const [wrapperDivId, setWrapperDivId] = useState(props.id ?? uuid.v4());
+        useEffect(() => {
+            setWrapperDivId(props.id ?? uuid.v4());
+        }, [props.id]);
+
+        // Styling
+        const { padding = 60, margin = 0, border = 'none', background } = props.options || {};
+        const viewConfig = props.viewConfig || {};
+        return (
+            <>
+                <div
+                    id={wrapperDivId}
+                    className={`gosling-component ${props.className || ''}`}
+                    style={{
+                        position: 'relative',
+                        padding: padding,
+                        margin: margin,
+                        border: border,
+                        background: background,
+                        width: props.size.width + padding * 2,
+                        height: props.size.height + padding * 2,
+                        textAlign: 'left'
+                    }}
+                >
+                    <div
+                        key={JSON.stringify(viewConfig)}
+                        className="higlass-wrapper"
+                        style={{
+                            position: 'relative',
+                            display: 'block',
+                            background: background,
+                            margin: 0,
+                            padding: 0, // non-zero padding acts unexpectedly w/ HiGlassComponent
+                            width: props.size.width,
+                            height: props.size.height
+                        }}
+                    >
+                        <HiGlassComponent
+                            ref={ref}
+                            options={{
+                                // bounded: true, // deprecated
+                                pixelPreciseMarginPadding: true, // this uses `rowHeight: 1` in react-grid-layout
+                                containerPaddingX: 0,
+                                containerPaddingY: 0,
+                                viewMarginTop: 0,
+                                viewMarginBottom: 0,
+                                viewMarginLeft: 0,
+                                viewMarginRight: 0,
+                                viewPaddingTop: 0,
+                                viewPaddingBottom: 0,
+                                viewPaddingLeft: 0,
+                                viewPaddingRight: 0,
+                                sizeMode: 'bounded',
+                                rangeSelectionOnAlt: true // this allows switching between `selection` and `zoom&pan` mode
+                            }}
+                            viewConfig={viewConfig}
+                        />
+                    </div>
+                </div>
+            </>
+        );
+    }
+);
