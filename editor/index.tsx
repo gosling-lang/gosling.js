bd5056add202fb9e15e9618a8de49bdc6f4079bf
Trevor Manz <trevor.j.manz@gmail.com>
2021-09-03 16:16:13 -0400

    chore: remove script tags for webpack & import higlass css

--- editor/index.tsx
+++ editor/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Editor from './editor';
 import './index.css';
+import 'higlass/dist/hglib.css';
 
 ReactDOM.render(
     <BrowserRouter>
