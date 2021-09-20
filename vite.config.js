0bf2e338540540dcaf6df5f791c971e3d8375279
Trevor Manz <trevor.j.manz@gmail.com>
2021-09-20 10:22:31 -0400

    chore: upgrade vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import * as esbuild from "esbuild";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import * as esbuild from 'esbuild';
+import path from 'path';
 
 /**
  * Bundles vite worker modules during development into single scripts.
@@ -9,55 +9,87 @@ import { resolve } from "path";
  * @returns {import('vite').Plugin}
  */
 const bundleWebWorker = () => {
-  return {
-    name: 'bundle-web-worker',
-    apply: 'serve', // plugin only applied with dev-server
-    async transform(_, id) {
-      if (/\?worker_file$/.test(id)) {
-        // just use esbuild to bundle the worker dependencies
-        const bundle = await esbuild.build({
-          entryPoints: [id],
-          inject: ['./src/alias/buffer-shim.js'],
-          format: 'esm',
-          bundle: true,
-          write: false,
-        });
-        if (bundle.outputFiles.length !== 1) {
-          throw new Error('Worker must be a single module.');
-        }
-        return bundle.outputFiles[0].text;
-      }
-    }
-  }
+    return {
+        name: 'bundle-web-worker',
+        apply: 'serve', // plugin only applied with dev-server
+        async transform(_, id) {
+            if (/\?worker_file$/.test(id)) {
+                // just use esbuild to bundle the worker dependencies
+                const bundle = await esbuild.build({
+                    entryPoints: [id],
+                    inject: ['./src/alias/buffer-shim.js'],
+                    format: 'esm',
+                    bundle: true,
+                    write: false,
+                });
+                if (bundle.outputFiles.length !== 1) {
+                    throw new Error('Worker must be a single module.');
+                }
+                return bundle.outputFiles[0].text;
+            }
+        },
+    };
 };
 
-export default defineConfig({
-  build: {
-    outDir: 'build',
-    minify: false,
-    target: 'es2018',
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es'],
+const alias = {
+    'gosling.js': path.resolve(__dirname, './src/index.ts'),
+    '@gosling.schema': path.resolve(__dirname, './src/core/gosling.schema'),
+    '@higlass.schema': path.resolve(__dirname, './src/core/higlass.schema'),
+    'zlib': path.resolve(__dirname, './src/alias/zlib.ts'),
+};
+
+const libEsm = defineConfig({
+    build: {
+        outDir: 'dist',
+        minify: true,
+        target: 'es2018',
+        emptyOutDir: false,
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            formats: ['esm'],
+        },
+        rollupOptions: {
+            external: [/^[^.\/]|^\.[^.\/]|^\.\.[^\/]/],
+        },
     },
-    rollupOptions: {
-      external: [/^[^.\/]|^\.[^.\/]|^\.\.[^\/]/],
-    }
-  },
-  resolve: {
-    alias: {
-      "gosling.js": resolve(__dirname, "./src/index.ts"),
-      "@gosling.schema": resolve(__dirname, "./src/core/gosling.schema"),
-      "@higlass.schema": resolve(__dirname, "./src/core/higlass.schema"),
-      "zlib": resolve(__dirname, "./src/alias/zlib.ts"),
-    },
-  },
-  define: {
-    "process.platform": "undefined",
-    "process.env.THREADS_WORKER_INIT_TIMEOUT": "undefined",
-  },
-  plugins: [
-      bundleWebWorker(),
-      reactRefresh(),
-  ],
+    resolve: { alias },
 });
+
+const libUmd = defineConfig({
+    build: {
+        outDir: 'dist',
+        minify: false,
+        target: 'es2018',
+        emptyOutDir: false,
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            formats: ['umd'],
+            name: 'gosling',
+        },
+        rollupOptions: {
+			output: { name: 'gosling' },
+            inlineDynamicImports: true,
+            external: ['react', 'react-dom', 'pixi.js', 'higlass'],
+        },
+    },
+    resolve: { alias },
+});
+
+const editorConfig = defineConfig({
+    build: { outDir: 'build' },
+    resolve: { alias },
+    define: {
+        'process.platform': 'undefined',
+        'process.env.THREADS_WORKER_INIT_TIMEOUT': 'undefined',
+    },
+    plugins: [
+        bundleWebWorker(),
+        reactRefresh(),
+    ],
+});
+
+export default ({ command, mode }) => {
+    if (command === 'build' && mode === 'lib-umd') return libUmd;
+    if (command === 'build' && mode === 'lib-esm') return libEsm;
+    return editorConfig;
+};
