a0759522887de86e9b360cf4d31af87587ab35f7
wangqianwen0418 <wangqianwen0418@163.com>
2021-10-10 21:27:17 -0400

    chore(docs): refine schema comments

--- src/core/gosling.schema.ts
+++ src/core/gosling.schema.ts
@@ -834,7 +834,7 @@ export interface DisplaceTransform {
         /** The name of a nominal field to group rows by in prior to piling-up */
         groupField?: string;
     };
-    //** A string that specifies the type of diseplancement, one of `"pile"` and `"spread"`  */
+    /** A string that specifies the type of diseplancement, one of `"pile"` and `"spread"`  */
     method: DisplacementType;
     newField: string;
 
@@ -845,7 +845,6 @@ export interface DisplaceTransform {
 export interface ExonSplitTransform {
     type: 'exonSplit';
     separator: string;
-    /** `{ field: string; value: number | string }` */
     flag: { field: string; value: number | string };
     fields: { field: string; type: FieldType; newField: string; chrField: string }[];
 }
