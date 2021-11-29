fafac4400fa22f5ced49938011d65f081dbeb066
Sehi L'Yi <sehilyi@gmail.com>
2021-11-29 17:08:43 -0500

    chore: resolve pkg name and version

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-export { name, version } from '../package.json';
+import pkg from '../package.json';
 import GoslingSchema from '../schema/gosling.schema.json';
 import ThemeSchema from '../schema/theme.schema.json';
 
+export const { name, version } = pkg;
+export { GoslingSchema, ThemeSchema };
 export type { GoslingSpec, TemplateTrackDef } from './core/gosling.schema';
 export type { HiGlassSpec } from './core/higlass.schema';
 export { GoslingTemplates } from './core/utils/template';
 export type { Theme } from './core/utils/theme';
-export { GoslingSchema };
-export { ThemeSchema };
 
 export { init } from './core/init';
 export { compile } from './core/compile';
