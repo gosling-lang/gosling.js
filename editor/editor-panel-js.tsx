858660390e6ff33fc89fc7c632a84c04376f28ef
wangqianwen0418 <wangqianwen0418@163.com>
2022-05-01 14:56:32 -0400

    fix: monaco worker

--- editor/editor-panel-js.tsx
+++ editor/editor-panel-js.tsx
@@ -3,31 +3,8 @@ import MonacoEditor from 'react-monaco-editor';
 
 import ReactResizeDetector from 'react-resize-detector';
 
-import 'monaco-editor/esm/vs/language/json/monaco.contribution';
-import 'monaco-editor/esm/vs/language/typescript/monaco.contribution';
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-import jsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
-import defaultWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-export * from 'monaco-editor/esm/vs/editor/edcore.main';
+export * from './monaco_worker';
 import * as Monaco from 'monaco-editor';
-
-// @ts-ignore
-// https://github.com/Microsoft/monaco-editor/blob/main/docs/integrate-esm.md#using-vite
-// A workaround: have to write the worker at two editors (js editor and json editor) seperatedly to make them work.
-// TODO: Not sure what causes this bug though.
-self.MonacoEnvironment = {
-    getWorker(id: string, label: string) {
-        switch (label) {
-            case 'json':
-                return new jsonWorker();
-            case 'typescript':
-            case 'javascript':
-                return new jsWorker();
-            default:
-                return new defaultWorker();
-        }
-    }
-};
 
 function EditorPanelJavascript(props: {
     code: string;
