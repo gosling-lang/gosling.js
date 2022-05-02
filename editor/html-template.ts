e27742db18caee76d83a9fb40bb1c3d635b90d49
Sehi L'Yi <sehilyi@gmail.com>
2022-05-02 15:12:50 -0400

    chore: remove styles

--- editor/html-template.ts
+++ editor/html-template.ts
@@ -14,12 +14,11 @@ export const getHtmlTemplate = (spec: string) => `
     <script src="https://unpkg.com/pixi.js@${pixiVersion}/dist/browser/pixi.min.js"></script>
     <script src="https://unpkg.com/higlass@${higlassVersion}/dist/hglib.js"></script>
     <script src="https://unpkg.com/gosling.js@${goslingVersion}/dist/gosling.js"></script>
-    <style>html, body, #gosling-container { width: 100%; height: 100% }</style>
 </head>
 <body>
     <div id="gosling-container"/>
     <script>
-    gosling.embed(document.getElementById('gosling-container'), ${spec})
+        gosling.embed(document.getElementById('gosling-container'), ${spec})
     </script>
 </body>
 </html>
