abb607a69d9d3f7a77bfa4c8c5e86b367ebd424d
Sehi L'Yi <sehilyi@gmail.com>
2022-05-12 14:43:16 -0400

    feat: support VCF with Tabix

--- src/gosling-track/gosling-track.ts
+++ src/gosling-track/gosling-track.ts
@@ -25,22 +25,23 @@ import {
     inferSvType
 } from '../core/utils/data-transform';
 import { getTabularData } from './data-abstraction';
-import { BAMDataFetcher } from '../data-fetcher/bam';
 import { getRelativeGenomicPosition } from '../core/utils/assembly';
 import { getTextStyle } from '../core/utils/text-style';
 import { Is2DTrack, IsChannelDeep, IsXAxis } from '../core/gosling.schema.guards';
 import { spawn } from 'threads';
-
-import BamWorker from '../data-fetcher/bam/bam-worker.js?worker&inline';
 import { InteractionEvent } from 'pixi.js';
 import { HIGLASS_AXIS_SIZE } from '../core/higlass-model';
+import { BAMDataFetcher } from '../data-fetcher/bam';
+import { GoslingVcfData } from '../data-fetcher/vcf';
+import BamWorker from '../data-fetcher/bam/bam-worker.js?worker&inline';
+import VcfWorker from '../data-fetcher/vcf/vcf-worker.js?worker&inline';
 
 // Set `true` to print in what order each function is called
 export const PRINT_RENDERING_CYCLE = false;
 
 // Experimental function to test with prerelease rendering
 function usePrereleaseRendering(spec: SingleTrack | OverlaidTrack) {
-    return spec.data?.type === 'bam';
+    return spec.data?.type === 'bam' || spec.data?.type === 'vcf';
 }
 
 type LoadingStage = 'loading' | 'processing' | 'rendering';
@@ -76,11 +77,16 @@ function GoslingTrack(HGC: any, ...args: any[]): any {
             const [context, options] = params;
 
             // Check whether to load a worker
-            let bamWorker;
+            let dataWorker;
             if (usePrereleaseRendering(options.spec)) {
                 try {
-                    bamWorker = spawn(new BamWorker());
-                    context.dataFetcher = new BAMDataFetcher(HGC, context.dataConfig, bamWorker);
+                    if (options.spec.data?.type === 'bam') {
+                        dataWorker = spawn(new BamWorker());
+                        context.dataFetcher = new BAMDataFetcher(HGC, context.dataConfig, dataWorker);
+                    } else if (options.spec.data?.type === 'vcf') {
+                        dataWorker = spawn(new VcfWorker());
+                        context.dataFetcher = new GoslingVcfData(HGC, context.dataConfig, dataWorker);
+                    }
                 } catch (e) {
                     console.warn('Error loading worker', e);
                 }
@@ -88,7 +94,7 @@ function GoslingTrack(HGC: any, ...args: any[]): any {
 
             super(context, options);
 
-            this.worker = bamWorker;
+            this.worker = dataWorker;
             context.dataFetcher.track = this;
             this.context = context;
 
@@ -453,7 +459,9 @@ function GoslingTrack(HGC: any, ...args: any[]): any {
                         this.tilesetInfo.tile_size
                     );
 
-                    const DEFAULT_MAX_TILE_WIDTH = 2e4; // base pairs
+                    // base pairs
+                    const DEFAULT_MAX_TILE_WIDTH =
+                        this.options.spec?.data?.type === 'bam' ? 2e4 : Number.MAX_SAFE_INTEGER;
 
                     if (tileWidth > (this.tilesetInfo.max_tile_width || DEFAULT_MAX_TILE_WIDTH)) {
                         this.forceDraw();
