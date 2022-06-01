fb661d29ae8cff768082356bee00a4dd077c205f
Trevor Manz <trevor.j.manz@gmail.com>
2022-06-01 17:16:08 -0400

    chore: bump vite to latest alpha

--- apps/editor/vite.config.js
+++ apps/editor/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react({ jsxRuntime: 'classic' })],
     optimizeDeps: {
         esbuildOptions: {
             inject: ['./buffer-shim.js']
