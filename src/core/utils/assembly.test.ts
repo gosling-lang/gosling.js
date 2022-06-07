3da1f48423326e4df9f45f636a364ab42f7a0a6e
Sehi L'Yi <sehilyi@gmail.com>
2022-06-07 12:52:02 -0400

    test: add a test for abs to rel genomic position

--- src/core/utils/assembly.test.ts
+++ src/core/utils/assembly.test.ts
@@ -1,4 +1,4 @@
-import { getChromInterval, getChromTotalSize, GET_CHROM_SIZES } from './assembly';
+import { getChromInterval, getChromTotalSize, getRelativeGenomicPosition, GET_CHROM_SIZES } from './assembly';
 import { CHROM_SIZE_HG38 } from './chrom-size';
 
 describe('Assembly', () => {
@@ -15,5 +15,11 @@ describe('Assembly', () => {
     });
     it('Chromosome interval calculation', () => {
         expect(getChromInterval({ 1: 111, 2: 222 })).toEqual({ 1: [0, 111], 2: [111, 333] });
+    });
+    it('Absolute to relative genomic position', () => {
+        expect(getRelativeGenomicPosition(CHROM_SIZE_HG38.chr1 + 1, 'hg38')).toEqual({
+            chromosome: 'chr2',
+            position: 1
+        });
     });
 });
