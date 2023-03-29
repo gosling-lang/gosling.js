ffe220bdd02bac0cb2e4fb601a19274723e3fdf1
etowahadams <etowahadams@gmail.com>
2023-03-29 14:45:03 -0400

    feat(editor): Make link to gosling-lang.org open in new tab
    Co-authored-by: Sehi L'Yi <sehilyi@gmail.com>

--- editor/editor.tsx
+++ editor/editor.tsx
@@ -776,7 +776,7 @@ function Editor(props: RouteComponentProps) {
                     </span>
                 ) : null}
                 <input type="hidden" id="spec-url-exporter" />
-                <a href="http://gosling-lang.org/" title="Go to the Gosling Project" className="mr-1">
+                <a href="http://gosling-lang.org/" title="Go to the Gosling Project" className="mr-1" target="_blank">
                     Gosling Project
                 </a>
                 {description ? (
