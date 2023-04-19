a6a3beee3a2ab4d209976ae8179b791efcb1991b
sehilyi <sehilyi@gmail.com>
2023-04-19 19:04:22 -0400

    chore: add snapshot generated

--- src/data-fetchers/vcf/vcf-data-fetcher.test.ts
+++ src/data-fetchers/vcf/vcf-data-fetcher.test.ts
@@ -21,6 +21,28 @@ describe('VCF file parser', () => {
         const prevPos = record.POS - 100;
         const tile = recordToTile(record, chrPos, prevPos);
 
-        expect(tile).toMatchInlineSnapshot();
+        expect(tile).toMatchInlineSnapshot(`
+          {
+            "AF": "0.5, DB, H2",
+            "ALT": "A",
+            "CHROM": "20",
+            "DISTPREV": 100,
+            "DISTPREVLOGE": 4.605170185988092,
+            "DP": "14",
+            "FILTER": "PASS",
+            "ID": [
+              "rs6054257",
+            ],
+            "INFO": "{\\"NS\\":[3],\\"DP\\":[14],\\"AF\\":[\\"0.5\\",\\"DB\\",\\"H2\\"]}",
+            "MUTTYPE": "substitution",
+            "NS": "3",
+            "ORIGINALPOS": 14370,
+            "POS": 137827,
+            "POSEND": 137828,
+            "QUAL": 29,
+            "REF": "G",
+            "SUBTYPE": "C>T",
+          }
+        `);
     });
 });
