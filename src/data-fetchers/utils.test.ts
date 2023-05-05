06d73e12b5d8308fe5449e17539b2eb6b1557600
sehilyi <sehilyi@gmail.com>
2023-05-05 16:38:43 -0400

    test: add tests

---
+++ src/data-fetchers/utils.test.ts
@@ -0,0 +1,55 @@
+import type { Datum } from '@gosling.schema';
+import { filterUsingGenoPos } from './utils';
+
+describe('Data Fetcher Utils', () => {
+    it('Filter data based on genomic window', () => {
+        const data: Datum[] = [
+            { x: '1', xe: '2' }, // outside
+            { x: '3', xe: '5' }, // partly overlap
+            { x: '5', xe: '6' }, // entirely inside
+            { x: '7', xe: '10' } // partly overlap
+        ];
+        const range: [number, number] = [4, 7];
+
+        // Filtering based on two genomic fields
+        expect(
+            filterUsingGenoPos(data, range, {
+                x: 'x',
+                xe: 'xe'
+            })
+        ).toMatchInlineSnapshot(`
+          [
+            {
+              "x": "3",
+              "xe": "5",
+            },
+            {
+              "x": "5",
+              "xe": "6",
+            },
+            {
+              "x": "7",
+              "xe": "10",
+            },
+          ]
+        `);
+
+        // Filtering based on a single genomic field
+        expect(
+            filterUsingGenoPos(data, range, {
+                x: 'x'
+            })
+        ).toMatchInlineSnapshot(`
+          [
+            {
+              "x": "5",
+              "xe": "6",
+            },
+            {
+              "x": "7",
+              "xe": "10",
+            },
+          ]
+        `);
+    });
+});
