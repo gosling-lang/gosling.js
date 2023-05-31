52bf2f27bece7a8ecc038533c8a051c62fb9ba7b
Trevor Manz <trevor.j.manz@gmail.com>
2023-05-31 15:58:17 -0400

    fix: revert breaking React 18 changes (#913)

--- editor/index.tsx
+++ editor/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Editor from './Editor';
 import './index.css';
 import 'higlass/dist/hglib.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+ReactDOM.render(
     <BrowserRouter>
         <Route component={Editor} />
-    </BrowserRouter>
+    </BrowserRouter>,
+    document.getElementById('root') as HTMLElement
 );
