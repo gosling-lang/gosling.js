171ad6f859f88301130b0cd57e90a962b37820a8
thomcsmits <thomcsmits@outlook.com>
2023-08-03 11:25:28 -0400

    feat(alt-text): create single track desc

--- src/alt-text/alt-from-spec.ts
+++ src/alt-text/alt-from-spec.ts
@@ -1,6 +1,6 @@
-import type { GoslingSpec, View, PartialTrack, RootSpecWithSingleView, ResponsiveSpecOfSingleView, RootSpecWithMultipleViews, ResponsiveSpecOfMultipleViews, ChannelValue, Encoding } from '../core/gosling.schema';
+import type { GoslingSpec, SingleTrack, View, PartialTrack, RootSpecWithSingleView, ResponsiveSpecOfSingleView, RootSpecWithMultipleViews, ResponsiveSpecOfMultipleViews, ChannelValue, Encoding } from '../core/gosling.schema';
 import type { GoslingSpecFixed, EncodingSeparated, TrackFixed, RootSpecWithSingleViewFixed, Counter, allSameValues, TrackSingleAlt, TrackMultipleAlt, TrackOverlaidAlt, AttributesAlt, GoslingSpecAlt, SingleTrackFixed } from './alt-gosling-schema';
-// import { attributeExists, attributeExistsAndChildHasValue} from './util';
+import { attributeExists, attributeHasChildValue, attributeExistsAndChildHasValue} from './util';
 import { determineSpecialCases } from './special-cases';
 // import { ExtendedSpecToAlt } from './write-alt';
 import {
@@ -23,6 +23,8 @@ export function getAlt(
 ): string {
     const altText = 'test';
     console.log(spec)
+
+    const altSpec = getAltSpec(spec);
     //let altSpecDef = AltExtendSpec(specCopy);
     // console.log(altSpecDef);
 
@@ -41,23 +43,31 @@ export function getAltSpec(
 ): GoslingSpecAlt {
     var altSpec = {} as GoslingSpecAlt;
     var counter = {"nTracks" : 0, "rowViews" : 0, "colViews" : 0};
+    determineStructure(spec, spec, counter)
 
     return altSpec;
 }
 
 export function determineStructure(
-    spec: GoslingSpec,
-    specPart: GoslingSpec
+    spec: any,
+    specPart: GoslingSpec,
+    counter: Counter,
 ): any {
     // singleview
     if ('tracks' in specPart) { 
         // multiple tracks
         if (specPart.tracks.length > 1) {
+            if (IsOverlaidTracks(specPart)) {
+
+            } 
+
             specPart.tracks.forEach(t => {
                 
             });
         } else {
-            // one track
+            const track = specPart.tracks[0] as SingleTrack;
+            console.log(specPart.tracks)
+            altSingleTrack(track, counter);
         }
     }
     // multiview
@@ -68,6 +78,60 @@ export function determineStructure(
 
 function altFlatTracks() {
 
+}
+
+function altSingleTrack(
+    track: SingleTrack,
+    // specAlt: GoslingSpecAlt,
+    //  savedAttributes: AttributesAlt, 
+    counter: Counter
+) {
+    var trackSingle = {} as TrackSingleAlt;
+
+    trackSingle.trackNumber = counter.nTracks;
+    trackSingle.rowNumber = counter.rowViews;
+    trackSingle.colNumber = counter.colViews;
+
+    trackSingle.title = track.title;
+    trackSingle.assembly = track.assembly;
+    trackSingle.layout = track.layout;
+    trackSingle.mark = track.mark;
+
+    trackSingle.encodingSeparated = checkEncodings(track);
+    trackSingle.data = track.data;
+
+    trackSingle.specialDesc = determineSpecialCases(trackSingle);
+
+    console.log(trackSingle)
+
+//     return trackSingle;
+    
+}
+
+
+function checkEncodings(
+    track: SingleTrack
+): EncodingSeparated {
+
+    var encodingField = {} as Encoding;
+    var encodingStatic = {} as Encoding;
+
+    const supportedEncodings = ['x', 'y', 'xe', 'ye', 'x1', 'y1', 'x1e', 'y1e', 'row', 'color', 'size', 'text', 'stroke', 'strokeWidth', 'opacity'];
+
+    for (const i in supportedEncodings) {
+        const encoding = supportedEncodings[i];
+        if (attributeExists(track, encoding)) {
+            if(attributeExists(track[encoding],'field')) {
+                encodingField[encoding] = track[encoding];
+            } else {
+                encodingStatic[encoding] = track[encoding];
+            }
+        }
+    }
+    
+    // bundle together into one object
+    const encodingSeparated: EncodingSeparated = {encodingField: encodingField, encodingStatic: encodingStatic}
+    return encodingSeparated;
 }
 
 function altStackedTracks() {
--- src/alt-text/util.ts
+++ src/alt-text/util.ts
@@ -2,6 +2,10 @@ export function attributeExists(object: any, attribute: any) {
     return(attribute in object);
 };
 
+export function attributeHasChildValue(object: any, attribute: any, attributeChild: any, value: any) {
+    return(object[attribute][attributeChild] === value);
+};
+
 export function attributeExistsAndChildHasValue(object: any, attribute: any, attributeChild: any, value: any) {
-    return(attribute in object && object[attribute][attributeChild] === value);
+    return(attributeExists(object, attribute) && attributeHasChildValue(object, attribute, attributeChild, value));
 };
