5df65fb3d5e8571d227eca4f20499b440dac11a3
thomcsmits <thomcsmits@outlook.com>
2023-08-08 01:36:59 -0400

    feat(alt-text): temporary tree for build

--- src/alt-text/alt-tree.tsx
+++ src/alt-text/alt-tree.tsx
@@ -2,76 +2,19 @@ import * as React from 'react';
 import type { AltGoslingSpec, AltTrack } from 'src/alt-text/alt-gosling-schema';
 
 
-export function createTree(data: AltGoslingSpec) {
-
-    return(
-        <div>
-            <ul className = 'alt-tree'>
-                <li className = 'alt-single'>
-                    Alt text: {data.alt}
-                </li>
-
-                <li className = 'alt-single'>
-                    Long description: {data.longDescription}
-                </li>
-
-                {data.title ? (
-                    <li className = 'alt-single'>
-                        Title: '{data.title}'
-                    </li>
-                ): null}
-                
-                {data.subtitle ? (
-                    <li className = 'alt-single'>
-                     Subtitle: '{data.subtitle}'
-                    </li>
-                ): null}
-
-                <li className = 'alt-parent'>
-                    Composition
-                    <ul>
-                        <li className = 'alt-single'>
-                            Description: {data.composition.description}
-                        </li>
-                        <li className = 'alt-single'>
-                            Number of tracks: {data.composition.nTracks}
-                        </li>             
-                    </ul>
-                </li>
-
-                <li>
-                    Tracks
-                    <ul>
-                        <li>
-                            0
-                            {createTreeTrack(data.tracks[0])}
-                        </li>
-                    </ul>
-                    
-                     {/* {Object.keys(data.tracks).map(t => 
-                    <li>
-                        {data.tracks[t]}
-                        {createTreeTrack(t)}
-                    </li>
-                    )} */}
-                </li>
-
-            </ul>
-        </div>
-    )
-}
-
-
-function createTreeTrack(t: AltTrack) {
+// Will be replaced with @mui tree once merged with thomcsmits/alt-text-tree-mui
+export function createTree(data: any) {
 
     return(
         <ul>
-            <li>
-                Track description: {t.description}
-            </li>
-            <li>
-                Track position: {t.position.description}
-            </li>
+            {Object.keys(data).map(key => (
+                <li>
+                    {key}
+                    {typeof(data[key]) === 'object' ? (
+                        createTree(data[key])
+                    ): null}
+                </li>
+            ))}
         </ul>
     )
 }
