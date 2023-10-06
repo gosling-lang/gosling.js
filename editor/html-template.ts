ed8caa72bb372a1f9ce134a48bf10e1f05a1f09e
etowahadams <etowahadams@gmail.com>
2023-10-06 18:01:45 -0400

    chore: update html-template

--- editor/html-template.ts
+++ editor/html-template.ts
@@ -1,7 +1,5 @@
 import { version as goslingVersion } from '../package.json';
-const higlassVersion = '1.12';
-const reactVersion = '17';
-const pixiVersion = '6';
+import { version as higlassVersion } from 'higlass/package.json';
 
 export const getHtmlTemplate = (spec: string) => `
 <!DOCTYPE html>
@@ -9,25 +7,12 @@ export const getHtmlTemplate = (spec: string) => `
 <head>
     <title>Gosling Visualization</title>
     <link rel="stylesheet" href="https://esm.sh/higlass@${higlassVersion}/dist/hglib.css">
-
-    <script type="importmap">
-    {
-    "imports": {
-        "react": "https://esm.sh/react@${reactVersion}?bundle",
-        "react-dom": "https://esm.sh/react-dom@${reactVersion}?bundle",
-        "pixi.js": "https://esm.sh/pixi.js@${pixiVersion}?bundle",
-        "higlass": "https://esm.sh/higlass@${higlassVersion}?bundle",
-        "gosling.js": "https://esm.sh/gosling.js@${goslingVersion}?bundle"
-    }
-    }
-    </script>
     
-
 </head>
 <body>
     <div id="gosling-container"></div>
     <script type="module">
-        import { embed } from 'gosling.js';
+        import { embed } from 'https://esm.sh/gosling.js@${goslingVersion}?bundle';
         embed(document.getElementById('gosling-container'), ${spec})
     </script>
 </body>
