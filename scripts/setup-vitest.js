8ec66100392948187d05e187e390d0a9f1dd1480
etowahadams <etowahadams@gmail.com>
2023-10-06 10:45:42 -0400

    fix: mock crypto using stubGlobal

--- scripts/setup-vitest.js
+++ scripts/setup-vitest.js
@@ -26,7 +26,15 @@ apis.forEach(api => {
 
 beforeAll(() => {
     // jsdom doesn't come with a `URL.createObjectURL` implementation
-    global.URL.createObjectURL = () => { return ''; };
+    global.URL.createObjectURL = () => {
+        return '';
+    };
+    // jsdom doesn't come with a `crypto.getRandomValues` implementation. This is for uuid. 
+    vi.stubGlobal('crypto', {
+        getRandomValues: function (buffer) {
+            return randomFillSync(buffer);
+        }
+    });
 });
 
 afterAll(() => {
