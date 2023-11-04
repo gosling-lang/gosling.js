510154d11583c1915a4ae9bd83486f9417d633c2
etowahadams <etowahadams@gmail.com>
2023-11-04 00:20:54 -0400

    feat: zoom

--- e2e/perf.spec.ts
+++ e2e/perf.spec.ts
@@ -14,9 +14,9 @@ function delay(time: number) {
 /**
  * Compares two PNG files and returns true if they are the same.
  */
-function isPngSame(newImg: Buffer, oldImgPath: string) {
+function isPngSame(newImg: Buffer, oldImg: Buffer) {
     const img1 = PNG.sync.read(newImg);
-    const img2 = PNG.sync.read(fs.readFileSync(oldImgPath));
+    const img2 = PNG.sync.read(oldImg);
     // check if the images have the same dimensions
     if (img1.width !== img2.width || img1.height !== img2.height) return false;
 
@@ -31,78 +31,101 @@ function isPngSame(newImg: Buffer, oldImgPath: string) {
  * This function changes the editor spec by pasting the given JSON string.
  */
 async function changeEditorSpec(page: Page, jsonString: string) {
+    // Copy the spec to the keyboard using the clipboard API
     await page.evaluate(jsonString => {
         navigator.clipboard.writeText(jsonString);
-        
     }, jsonString);
+    // click into the text editor
     await page.mouse.click(200, 200);
-    await page.getByRole('textbox', { name: 'Editor content;Press Alt+F1 for Accessibility Options.' }).press('Control+KeyA');
+    // Control+A to select all
+    await page
+        .getByRole('textbox', { name: 'Editor content;Press Alt+F1 for Accessibility Options.' })
+        .press('Control+KeyA');
+    // Backspace to delete what is in the text editor
     await page.keyboard.press('Backspace');
 
     await delay(100);
+    // Right click to pull up menu
     await page.mouse.click(200, 200, { button: 'right' });
     await delay(100); // this is needed to wait for the context menu to appear
+    // Click on the paste button
     await page.getByRole('menuitem', { name: 'Paste' }).click();
 }
 
 /**
  * This function polls until the screenshot of the given component matches the expected screenshot.
  */
-async function pollUntilScreenshotMatchesExpected(
-    component: Locator,
-    page: Page,
-    expectedScreenshotPath: string,
-    timeout: number
-) {
+async function checkScreenshotUntilMatches(component: Locator, expectedScreenshotPath: string, timeout: number) {
     let screenshotMatchesExpected = false;
     let timeElapsed = 0;
+    const compImgBuffer = fs.readFileSync(expectedScreenshotPath);
+
     while (!screenshotMatchesExpected && timeElapsed < timeout) {
         const screenshot = await component.screenshot();
 
-        screenshotMatchesExpected = isPngSame(screenshot, expectedScreenshotPath);
+        screenshotMatchesExpected = isPngSame(screenshot, compImgBuffer);
 
         if (!screenshotMatchesExpected) {
-            await page.waitForTimeout(50); // wait 50ms before polling again
+            await delay(50); // wait 10ms before polling again
             timeElapsed += 50;
         }
     }
-    return timeElapsed;
 }
 
-test('changes editor spec', async ({ page, context }) => {
+async function getTotalBlockingTime(page: Page) {
+    const blockingTime = await page.evaluate(() => {
+        return new Promise(resolve => {
+            let totalBlockingTime = 0;
+            new PerformanceObserver(function (list) {
+                const perfEntries = list.getEntries();
+
+                perfEntries.forEach(perfEntry => {
+                    totalBlockingTime += perfEntry.duration - 50;
+                });
+
+                console.log(JSON.stringify(perfEntries));
+
+                resolve(totalBlockingTime);
+            }).observe({ type: 'longtask', buffered: true });
+
+            // Resolve promise if there haven't been long tasks
+            setTimeout(() => resolve(totalBlockingTime), 5000);
+        });
+    });
+    return blockingTime;
+}
+
+test.beforeEach(async ({ page, context }) => {
     await context.grantPermissions(['clipboard-read', 'clipboard-write']);
     await page.goto('/');
+    // await page.waitForLoadState('networkidle');
+});
+
+test('changes editor spec', async ({ page, browser }) => {
+    // await browser.startTracing(page, { path: './perfTraces.json', screenshots: false })
     await changeEditorSpec(page, jsonString);
     // wait for network to go idle
-    await page.waitForLoadState('networkidle');
+    // await page.waitForLoadState('networkidle');
     const gosComponent = page.getByLabel('Gosling visualization');
 
-    const matchTime = await pollUntilScreenshotMatchesExpected(
+    console.time('Time until screenshot matches expected');
+    await checkScreenshotUntilMatches(
         gosComponent,
-        page,
         'e2e/perf.spec.ts-snapshots/changes-editor-spec-1-chromium-darwin.png',
         10000
     );
+    console.timeEnd('Time until screenshot matches expected');
 
-    const totalBlockingTime = await page.evaluate(() => {
-        return new Promise((resolve) => {
-          let totalBlockingTime = 0
-          new PerformanceObserver(function (list) {
-            const perfEntries = list.getEntries()
-            for (const perfEntry of perfEntries) {
-              totalBlockingTime += perfEntry.duration - 50
-            }
-            perfEntries.forEach((entry) => console.log(entry.toJSON()))
-            resolve(totalBlockingTime)
-          }).observe({ type: 'longtask', buffered: true })
+    await delay(1000);
+    const centerTrack: Locator = page.locator('.center-track').first();
+    await centerTrack.hover();
     
-          // Resolve promise if there haven't been long tasks
-          setTimeout(() => resolve(totalBlockingTime), 5000)
-        })
-      })
-    
-    console.log('blocking', parseFloat(totalBlockingTime as string)) // 0
-    console.log(matchTime);
-    await delay(10000);
-    
+    for (let i = 0; i < 20; i++) {
+        await page.mouse.wheel(0, -100);
+    }
+
+    await delay(1000);
+    const blockingTime = await getTotalBlockingTime(page);
+    console.log(blockingTime);
+    // await browser.stopTracing()
 });
