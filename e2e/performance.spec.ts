1c75fa138bbef8d497f3d12e5996f0b95c506468
etowahadams <etowahadams@gmail.com>
2023-11-09 02:24:14 +0900

    test: see why playwright CI is breaking

--- e2e/performance.spec.ts
+++ e2e/performance.spec.ts
@@ -14,12 +14,16 @@ test('Measure zoom time', async ({ page, browser }) => {
     await changeEditorSpec(page, jsonString);
     
     // Wait for the visualization to render 
+    await delay(5000);
+    // Optionally, wait for the visualization to render by checking the screenshot. However, this does 
+    
     const gosComponent = page.getByLabel('Gosling visualization');
-    await checkScreenshotUntilMatches(
-        gosComponent,
-        'e2e/assets/example-spec-expected.png',
-        10000
-    );
+    await gosComponent.screenshot({ path: 'e2e/assets/example-spec-expected-2.png' });
+    // await checkScreenshotUntilMatches(
+    //     gosComponent,
+    //     'e2e/assets/example-spec-expected.png',
+    //     10000
+    // );
 
     // Hover over a track
     await delay(1000);
