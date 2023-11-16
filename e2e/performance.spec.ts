ec215c0fc8cc944e299327ddb1d1ebfa611b6dcc
etowahadams <etowahadams@gmail.com>
2023-11-16 16:22:01 -0500

    test: adjust time

--- e2e/performance.spec.ts
+++ e2e/performance.spec.ts
@@ -47,6 +47,6 @@ test('Measure zoom time', async ({ page, browser }, testInfo) => {
     const zoomTime = endTime - startTime;
     console.log(`Zoom time: ${zoomTime}ms`);
 
-    // Just make sure the zoom time is less than 3 seconds. In practice it should be much less than this.
-    expect(zoomTime).toBeLessThan(5000);
+    // Just make sure the zoom time is less than 8 seconds. This is how long it in CI
+    expect(zoomTime).toBeLessThan(8000);
 });
