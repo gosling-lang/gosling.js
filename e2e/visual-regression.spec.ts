34ca7680f1882a96b58387f4ba0f4b9c28dde45f
etowahadams <etowahadams@gmail.com>
2023-11-21 18:23:56 -0500

    test: update snapshots

--- e2e/visual-regression.spec.ts
+++ e2e/visual-regression.spec.ts
@@ -1,7 +1,7 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 import { JsonExampleSpecs } from '../editor/example/json-spec';
-import { changeEditorSpec, delay, isPngSame } from './utils';
+import { changeEditorSpec } from './utils';
 
 test.beforeEach(async ({ page, context }) => {
     // Enable clipboard permissions. This is needed to copy the spec to the clipboard in the chromium browser.
@@ -50,21 +50,32 @@ test.beforeEach(async ({ page, context }) => {
 // });
 
 Object.entries(JsonExampleSpecs)
-    // .filter(([name]) => name === 'doc_text') // we only want to see the broken example now
+    // .filter(([name]) => name === 'doc_text') // If only want to see the certain example
     .forEach(([name, jsonSpec]) => {
         test(name, async ({ page }, testInfo) => {
             test.setTimeout(60000); // 60 seconds
 
             let spec = JSON.stringify(jsonSpec);
             await changeEditorSpec(page, spec);
-            await page.waitForTimeout(10000);
+            
             const gosComponent = page.locator('.gosling-component').first();
+            // Wait 10 seconds for the visualization to render
+            await page.waitForTimeout(20000);
+            // Then take a screenshot
+            await expect(gosComponent).toHaveScreenshot();
+            
+            // const screenshot = await gosComponent.screenshot();
+            // const diff = getPngDiff(screenshot, screenshot);
+            // if (diff) {
+            //     await testInfo.attach(`${name}_diff`, {
+            //         body: diff,
+            //         contentType: 'image/png'
+            //     });
+            // }
 
-            // Uncomment this to see the screenshot of the component in the report
-            const screenshot = await gosComponent.screenshot();
-            await testInfo.attach(`${name}_screenshot`, {
-                body: screenshot,
-                contentType: 'image/png'
-            });
+            // await testInfo.attach(`${name}_screenshot`, {
+            //     body: screenshot,
+            //     contentType: 'image/png'
+            // });
         });
     });
