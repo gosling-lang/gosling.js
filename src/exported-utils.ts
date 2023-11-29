0cb7de44db436864f89ab4324049f11e2af43692
Trevor Manz <trevor.j.manz@gmail.com>
2023-11-29 15:19:45 -0500

    feat: Create separate entrypoint for gosling.js/utils

---
+++ src/exported-utils.ts
@@ -0,0 +1,8 @@
+export {
+    getRelativeGenomicPosition,
+    computeChromSizes,
+    getChromInterval,
+    getChromTotalSize,
+    parseGenomicPosition
+} from './core/utils/assembly';
+export { sanitizeChrName } from './data-fetchers/utils';
