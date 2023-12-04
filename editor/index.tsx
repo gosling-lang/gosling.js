3bd9607167ce635d707a952c8b2435f191e27baf
etowahadams <etowahadams@gmail.com>
2023-12-04 10:20:57 -0500

    chore(core): upgrade higlass (#1003)
    * chore: upgrade higlass
    
    * chore: css import changes

--- editor/index.tsx
+++ editor/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Editor from './Editor';
 import './index.css';
-import 'higlass/dist/hglib.css';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
