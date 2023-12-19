85f0be1efcac6db4e2e3e95cb6665e544b1e91f1
Thomas C. Smits <90332819+thomcsmits@users.noreply.github.com>
2023-12-19 17:19:33 +0100

    feat(core): export util functions for conversion to flat tracks (#1016)
    * feat: export convertToFlatTrack+spreadTracksByData
    
    * chore(core): make it clearer that they are internal/experimental functions (#1017)
    
    chore: make it internal
    
    Co-authored-by: Sehi L'Yi <sehliyi@gmail.com>
    
    ---------
    
    Co-authored-by: Sehi L'Yi <sehilyi@gmail.com>
    Co-authored-by: Sehi L'Yi <sehliyi@gmail.com>

--- src/exported-utils.ts
+++ src/exported-utils.ts
@@ -6,3 +6,7 @@ export {
     parseGenomicPosition
 } from './core/utils/assembly';
 export { sanitizeChrName } from './data-fetchers/utils';
+
+// These are experimental and may be removed in the future
+export { convertToFlatTracks as _convertToFlatTracks } from './compiler/spec-preprocess';
+export { spreadTracksByData as _spreadTracksByData } from './core/utils/overlay';
