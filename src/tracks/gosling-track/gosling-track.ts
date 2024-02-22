560d1210fe9d8f4d7326a9c0dc4a75b5b9254614
etowahadams <etowahadams@gmail.com>
2024-02-22 10:25:22 -0500

    perf(track): Persist GoslingTrackModels (#1044)
    * perf: persist GoslingTrackModels
    
    * perf: replace structuredClone

--- src/tracks/gosling-track/gosling-track.ts
+++ src/tracks/gosling-track/gosling-track.ts
@@ -174,6 +174,8 @@ const factory: PluginTrackFactory<Tile, GoslingTrackOptions> = (HGC, context, op
         #loadingText = new HGC.libraries.PIXI.Text('', loadingTextStyle);
         prevVisibleAndFetchedTiles?: Tile[];
         resolvedTracks: SingleTrack[] | undefined;
+        // This is used to persist processed tile data across draw() calls.
+        #processedTileMap: WeakMap<Tile, boolean> = new WeakMap();
 
         /* *
          *
@@ -416,6 +418,8 @@ const factory: PluginTrackFactory<Tile, GoslingTrackOptions> = (HGC, context, op
             this.getResolvedTracks(true); // force update
             this.clearMouseEventData();
             this.textsBeingUsed = 0;
+            // Without this, tracks with the same ID between specs will not be redrawn
+            this.#processedTileMap = new WeakMap();
 
             this.processAllTiles(true);
             this.draw();
@@ -555,6 +559,11 @@ const factory: PluginTrackFactory<Tile, GoslingTrackOptions> = (HGC, context, op
             this.tileSize = this.tilesetInfo?.tile_size ?? 1024;
 
             const tiles = this.visibleAndFetchedTiles();
+            // If we have already processed all tiles, we don't need to do anything
+            // this.#processedTileMap contains all of data needed to draw
+            if (tiles.every(tile => this.#processedTileMap.get(tile) !== undefined)) {
+                return;
+            }
 
             // generated tabular data
             tiles.forEach(tile => this.#generateTabularData(tile, force));
@@ -571,6 +580,11 @@ const factory: PluginTrackFactory<Tile, GoslingTrackOptions> = (HGC, context, op
             if (flatTileData.length !== 0) {
                 this.options.siblingIds.forEach(id => publish('rawData', { id, data: flatTileData }));
             }
+
+            // Record processed tiles so that we don't process them again
+            tiles.forEach(tile => {
+                this.#processedTileMap.set(tile, true);
+            });
         }
 
         /**
