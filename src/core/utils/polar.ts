3d7213b61ce1381c793080bbd4f075c95ac6bba4
etowahadams <etowahadams@gmail.com>
2024-06-11 14:50:38 -0400

    feat: polar clockwise option

--- src/core/utils/polar.ts
+++ src/core/utils/polar.ts
@@ -4,21 +4,35 @@ export const RADIAN_GAP = 0; //0.04;
  * Convert a value in a single-linear axis to a radian value. Anticlockwise, starts from 12 o'clock.
  * v span from zero to `max`.
  */
-export function valueToRadian(v: number, max: number, sa: number, ea: number, g?: number) {
+export function valueToRadian(v: number, max: number, sa: number, ea: number, clockwise?: boolean) {
     const safeVal = Math.max(Math.min(max, v), 0);
-    const gap = g ?? RADIAN_GAP;
+    const gap = RADIAN_GAP;
     const radExtent = ((ea - sa) / 360) * Math.PI * 2 - gap * 2;
     const radStart = (sa / 360) * Math.PI * 2;
-    return -(radStart + (safeVal / max) * radExtent) - Math.PI / 2.0 - gap;
+    const result = -(radStart + (safeVal / max) * radExtent) - Math.PI / 2.0 - gap;
+
+    if (clockwise) {
+        return -result - Math.PI;
+    }
+    return result;
 }
 
 /**
  * Convert a position in a cartesian system to a polar coordinate.
  */
-export function cartesianToPolar(x: number, max: number, r: number, cx: number, cy: number, sa: number, ea: number) {
+export function cartesianToPolar(
+    x: number,
+    max: number,
+    r: number,
+    cx: number,
+    cy: number,
+    sa: number,
+    ea: number,
+    clockwise?: boolean
+) {
     return {
-        x: cx + r * Math.cos(valueToRadian(x, max, sa, ea)),
-        y: cy + r * Math.sin(valueToRadian(x, max, sa, ea))
+        x: cx + r * Math.cos(valueToRadian(x, max, sa, ea, clockwise)),
+        y: cy + r * Math.sin(valueToRadian(x, max, sa, ea, clockwise))
     };
 }
 
