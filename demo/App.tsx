186ebd149eb50a64f114028bcd3e01fc7e843d51
etowahadams <etowahadams@gmail.com>
2024-06-12 15:56:49 -0400

    feat: dummy track example

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { PixiManager } from '@pixi-manager';
+import { addDummyTrack } from './examples/dummy-track';
 
 import './App.css';
 
@@ -12,6 +13,8 @@ function App() {
         plotElement.innerHTML = '';
         // Initialize the PixiManager. This will be used to get containers and overlay divs for the plots
         const pixiManager = new PixiManager(1000, 600, plotElement, setFps);
+        // Add the dummy track
+        addDummyTrack(pixiManager);
     }, []);
 
     return (
