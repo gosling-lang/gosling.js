1107a7ec9932f0c6d4eb96e1cc976a58cff3d152
etowahadams <etowahadams@gmail.com>
2024-06-12 14:37:31 -0400

    refactor: add react import

--- demo/main.tsx
+++ demo/main.tsx
@@ -1,7 +1,6 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
