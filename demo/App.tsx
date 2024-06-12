9f00ad86ab4c0f52cee2a40d47ba033896f584a7
etowahadams <etowahadams@gmail.com>
2024-06-12 14:33:59 -0400

    feat: add to demo

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,26 +1,28 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import { PixiManager } from '@pixi-manager';
 
+import './App.css';
 
 function App() {
-  const [fps, setFps] = useState(120);
+    const [fps, setFps] = useState(120);
 
-  useEffect(() => {
-    // Create the new plot
-    const plotElement = document.getElementById("plot") as HTMLDivElement;
-    plotElement.innerHTML = "";
+    useEffect(() => {
+        // Create the new plot
+        const plotElement = document.getElementById('plot') as HTMLDivElement;
+        plotElement.innerHTML = '';
+        // Initialize the PixiManager. This will be used to get containers and overlay divs for the plots
+        const pixiManager = new PixiManager(1000, 600, plotElement, setFps);
+    }, []);
 
-  }, []);
+    return (
+        <>
+            <h1>HiGlass/Gosling tracks with new renderer</h1>
 
-  return (
-    <>
-      <h1>HiGlass/Gosling tracks with new renderer</h1>
-
-      <div className="card">
-        <div className="card" id="plot"></div>
-      </div>
-    </>
-  );
+            <div className="card">
+                <div className="card" id="plot"></div>
+            </div>
+        </>
+    );
 }
 
 export default App;
