789f7cff406ab3f148829cc92f23014faefd5c85
etowahadams <etowahadams@gmail.com>
2024-07-06 17:25:18 -0400

    comment about types

--- demo/track-def/types.ts
+++ demo/track-def/types.ts
@@ -1,4 +1,17 @@
 import type { DataDeep, Assembly } from '@gosling-lang/gosling-schema';
+
+/**
+ * After the Gosling spec is compiled, it is a "processed spec".
+ * A processed spec has most of the same properties as the original spec, but some properties are
+ * added or modified during the compilation process.
+ *
+ * For example, a valid Gosling spec may have no 'id' property, but a processed spec will always have an 'id' property.
+ *
+ * This file contains the types for the processed spec.
+ *
+ * TODO: this file is incomplete. It should be updated to include all the properties that a processed spec can have.
+ */
+
 /** A Track after it has been compiled */
 export type ProcessedTrack = ProcessedLinearTrack | ProcessedCircularTrack;
 /** All tracks potentially have these properties */
