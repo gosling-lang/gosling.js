22b3caf8707e3a816f7193b26c67dd88d399804a
Trevor Manz <trevor.j.manz@gmail.com>
2025-03-11 20:36:57 -0400

    chore(core): Upgrade vitest/vite and patch @gmod/bam (#1126)
    * Upgrade vite and vitest
    
    * Use pnpm patch package to patch @gmod/bam
    
    * Remove buffer shim
    
    * Update snapshots

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as esbuild from 'esbuild';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import pkg from './package.json' assert { type: 'json' }; // must do the assert to solve https://nodejs.org/api/errors.html#err_import_assertion_type_missing
@@ -8,61 +7,23 @@ import pkg from './package.json' assert { type: 'json' }; // must do the assert 
 const __dirname = fileURLToPath(dirname(import.meta.url));
 
 /**
- * Bundles vite worker modules during development into single scripts.
- * Also injects node globals for gmod libraries.
- * see: https://github.com/hms-dbmi/viv/pull/469#issuecomment-877276110
- * @returns {import('vite').Plugin}
+ * @param {typeof pkg} pkg
+ * @param {{ forceExclude: Array<string> }} options
  */
-const bundleWebWorker = {
-    name: 'bundle-web-worker',
-    apply: 'serve', // plugin only applied with dev-server
-    async transform(_, id) {
-        if (/\?worker_file$/.test(id)) {
-            // just use esbuild to bundle the worker dependencies
-            const bundle = await esbuild.build({
-                entryPoints: [id],
-                inject: ['./src/alias/buffer-shim.js'],
-                format: 'iife',
-                bundle: true,
-                write: false
-            });
-            if (bundle.outputFiles.length !== 1) {
-                throw new Error('Worker must be a single module.');
-            }
-            return bundle.outputFiles[0].text;
-        }
-    }
-};
+function determineExternalDependencies(pkg, options) {
+    // Packages that we have modified cannot be external because package consumers will not
+    // get our modifications. Ideally, we can avoid modifying these packages ourselves.
+    const patchedPackages = Object.keys(pkg.pnpm.patchedDependencies).map(
+        specifier => specifier.split('@').slice(0, specifier.startsWith('@') ? 2 : 1).join('@')
+    );
+    return [
+        ...Object.keys(pkg.dependencies),
+        ...Object.keys(pkg.peerDependencies)
+    ].filter(
+        dep => ![...patchedPackages, ...options.forceExclude].includes(dep)
+    );
 
-// audit libraries to see if they use `Buffer` in a way that needs to be shimmed
-
-// We can't inject a global `Buffer` polyfill for the worker entrypoint using vite alone,
-// so we reuse the `bundle-web-worker` plugin to inject the buffer shim during production.
-const manualInlineWorker = {
-    apply: 'build',
-    async transform(code, id) {
-        if (
-            id.endsWith('bam-worker.ts?worker&inline') ||
-            id.endsWith('vcf-worker.ts?worker&inline') ||
-            id.endsWith('gff-worker.ts?worker&inline') ||
-            id.endsWith('bed-worker.ts?worker&inline')
-        ) {
-            const bundle = await bundleWebWorker.transform(code, id + '?worker_file');
-            const base64 = Buffer.from(bundle).toString('base64');
-            // https://github.com/vitejs/vite/blob/72cb33e947e7aa72d27ed0c5eacb2457d523dfbf/packages/vite/src/node/plugins/worker.ts#L78-L87
-            return `const encodedJs = "${base64}";
-const blob = typeof window !== "undefined" && window.Blob && new Blob([atob(encodedJs)], { type: "text/javascript;charset=utf-8" });
-export default function() {
-  const objURL = blob && (window.URL || window.webkitURL).createObjectURL(blob);
-  try {
-    return objURL ? new Worker(objURL) : new Worker("data:application/javascript;base64," + encodedJs, {type: "module"});
-  } finally {
-    objURL && (window.URL || window.webkitURL).revokeObjectURL(objURL);
-  }
-}`;
-        }
-    }
-};
+}
 
 const alias = {
     'gosling.js': path.resolve(__dirname, './src/index.ts'),
@@ -74,14 +35,14 @@ const alias = {
     '@gosling-lang/gosling-brush': path.resolve(__dirname, './src/tracks/gosling-brush/index.ts'),
     '@gosling-lang/dummy-track': path.resolve(__dirname, './src/tracks/dummy-track/index.ts'),
     '@data-fetchers': path.resolve(__dirname, './src/data-fetchers/index.ts'),
-    zlib: path.resolve(__dirname, './src/alias/zlib.ts'),
-    stream: path.resolve(__dirname, './node_modules/stream-browserify') //  gmod/gff uses stream-browserify
+    zlib: path.resolve(__dirname, './src/alias/zlib.ts'), // gmod/bbi uses zlib
+    stream: 'stream-browserify' //  gmod/gff uses stream-browserify
 };
 
-const skipExt = new Set(['@gmod/bbi']);
-const external = [...Object.keys(pkg.dependencies), ...Object.keys(pkg.peerDependencies)].filter(
-    dep => !skipExt.has(dep)
-);
+const external = determineExternalDependencies(pkg, {
+    // see aliases above
+    forceExclude: ["@gmod/bbi", "@gmob/gff"],
+})
 
 const esm = defineConfig({
     build: {
@@ -101,7 +62,7 @@ const esm = defineConfig({
         rollupOptions: { external }
     },
     resolve: { alias },
-    plugins: [manualInlineWorker]
+    plugins: [],
 });
 
 const dev = defineConfig({
@@ -111,7 +72,7 @@ const dev = defineConfig({
         'process.platform': 'undefined', // because of the threads library relies on process global
         'process.env.THREADS_WORKER_INIT_TIMEOUT': 'undefined'
     },
-    plugins: [bundleWebWorker, manualInlineWorker]
+    plugins: [],
 });
 
 const testing = defineConfig({
@@ -137,7 +98,8 @@ const testing = defineConfig({
             reporter: ['lcov', 'text'],
             include: ['src', 'editor']
         }
-    }
+    },
+    plugins: []
 });
 
 export default ({ command, mode }) => {
