175da81719d3d50c4566ae22f6da483400d0c81e
SEHI L'YI <sehilyi@gmail.com>
2025-04-02 14:33:29 -0400

    feat: 3D model for a spatial view specified in the layout (#1131)
    * feat: minimal join data transform
    
    * chore: add a todo statement
    
    * first working demo
    
    * more work on bigwig join
    
    * first demo to load bigwig and full chromosomes
    
    * all d3 models drawn
    
    * chore: remove console log statements and clean up code
    
    * chore: fix typo in the comment statement
    
    * chore: more clean up
    
    * chore: compact axis
    
    * fix: fix loading json-based track
    
    * feat: grammar extension for 3D layout (#1136)
    
    feat: better grammar extension for 3d vis

--- demo/renderer/main.ts
+++ demo/renderer/main.ts
@@ -117,7 +117,7 @@ export function renderTrackDefs(
             // In the final version, we would probably want Chromospace to use an existing canvas element (to limit the creation of new elements).
             // But for now this gets the job done.
             const container = pixiManager.makeContainer(boundingBox).overlayDiv;
-            console.log("!@$!#%@#");
+            console.log('!@$!#%@#');
             console.log(options.spec);
             options.spec.data.sampleLength = 30000;
             const datafetcher = getDataFetcher(options.spec, urlToFetchOptions);
