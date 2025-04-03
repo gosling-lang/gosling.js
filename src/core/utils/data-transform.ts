8cf6b9863ca448e335f074050329a5b90ea9d4b1
sehilyi <sehilyi@gmail.com>
2025-04-03 17:15:39 -0400

    join correctly when there is only a single genomic field

--- src/core/utils/data-transform.ts
+++ src/core/utils/data-transform.ts
@@ -113,16 +113,19 @@ export async function joinData(
         return data;
     };
     const fromData = await fetchDataIfNeeded(from.url);
-
     // a very naive approach to join two files
     const joinned: Datum[] = fromData.map(f => {
         // TODO: to be most accurate, need to find all data records matching and aggregate data
         const found = toData.find(t => {
-            const start = +t[to.startField] <= +f[from.genomicField];
-            const end = to.endField ? +t[to.endField] >= +f[from.genomicField] : true;
-            return start && end;
+            if (to.endField) {
+                const start = +t[to.startField] <= +f[from.genomicField];
+                const end = +t[to.endField] >= +f[from.genomicField];
+                return start && end;
+            } else {
+                return +t[to.startField] == +f[from.genomicField];
+            }
         }) ?? { value: 0 };
-        return { ...found, ...f };
+        return { ...f, ...found };
     });
     return joinned;
 }
