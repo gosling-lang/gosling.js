ac1b680e31e0fef14a85433be306e45b6f07b152
David Kouřil <david.kouril@gmail.com>
2025-04-03 21:39:38 -0400

    Example: three different ways of setting color (#1144)
    * feat: add example of different coloring strategies
    
    constant vs nominal vs quantitative
    
    * chore: remove data fields in tracks since it's not needed anymore
    
    * chore: change the default model for minimal to stevens
    
    also reverted back to being really 'minimal'

--- editor/example/json-spec/spatial-layout.ts
+++ editor/example/json-spec/spatial-layout.ts
@@ -9,6 +9,32 @@ export const EX_SPEC_SPATIAL_MINIMAL: GoslingSpec = {
                 type: 'spatial',
                 model: {
                     type: 'csv',
+                    url: 'https://raw.githubusercontent.com/dvdkouril/chromospace-sample-data/refs/heads/main/gosling-3d/stevens-2017/full-model.csv',
+                    xyz: ['x', 'y', 'z'],
+                    chromosome: 'chr',
+                    position: 'coord'
+                }
+            },
+            tracks: [
+                {
+                    width: 500,
+                    height: 500
+                }
+            ]
+        },
+    ]
+};
+
+export const EX_SPEC_SPATIAL_COLOR: GoslingSpec = {
+    title: 'Coloring strategies',
+    subtitle: 'Showcase of different ways to use the color channel.',
+    arrangement: "horizontal",
+    views: [
+        {
+            layout: {
+                type: 'spatial',
+                model: {
+                    type: 'csv',
                     url: 'https://raw.githubusercontent.com/dvdkouril/chromospace-sample-data/main/gosling-3d/yeast_model.csv',
                     xyz: ['x', 'y', 'z'],
                     chromosome: 'chr',
@@ -17,11 +43,52 @@ export const EX_SPEC_SPATIAL_MINIMAL: GoslingSpec = {
             },
             tracks: [
                 {
-                    color: { field: 'coord', type: 'quantitative' },
-                    data: {
-                        type: 'csv',
-                        url: 'https://gist.githubusercontent.com/sehilyi/29d1cfed56da3ed37370f31a508c8024/raw/9ab733bff25c4c539d86451dfb9d03d4e241d1ad/yeast_model.values.csv',
-                        genomicFields: ['position']
+                    color: {
+                        value: "lightgreen",
+                    },
+                    width: 500,
+                    height: 500
+                }
+            ]
+        },
+        {
+            layout: {
+                type: 'spatial',
+                model: {
+                    type: 'csv',
+                    url: 'https://raw.githubusercontent.com/dvdkouril/chromospace-sample-data/main/gosling-3d/yeast_model.csv',
+                    xyz: ['x', 'y', 'z'],
+                    chromosome: 'chr',
+                    position: 'coord'
+                }
+            },
+            tracks: [
+                {
+                    color: {
+                        field: "chr",
+                        type: "nominal",
+                    },
+                    width: 500,
+                    height: 500
+                }
+            ]
+        },
+        {
+            layout: {
+                type: 'spatial',
+                model: {
+                    type: 'csv',
+                    url: 'https://raw.githubusercontent.com/dvdkouril/chromospace-sample-data/main/gosling-3d/yeast_model.csv',
+                    xyz: ['x', 'y', 'z'],
+                    chromosome: 'chr',
+                    position: 'coord'
+                }
+            },
+            tracks: [
+                {
+                    color: {
+                        field: "coord",
+                        type: "quantitative",
                     },
                     width: 500,
                     height: 500
